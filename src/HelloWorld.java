f9b23d2344dd1217bebbe015ce913ecb43d200d8
Brice Ernst <brice.ernst1@gmail.com>
2022-02-28 16:55:24 -0600

    syntax types variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,43 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+//
+//        int favoriteNumber = 87;
+//        System.out.println(favoriteNumber);
+//
+//        String myString = "Brice";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        System.out.println(x);
+//        y -= x;
+//        System.out.println(y);
+
+        int max = Integer.MAX_VALUE;
+        System.out.println(max++);
+        System.out.println(max);
     }
 }
